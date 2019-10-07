ad8c1bf9604cd92c943b12f543af00cb0ab0c7cd
Warren <weber767@umn.edu>
2019-10-07 16:42:52 -0500

    current ver 1.1

--- docs/pass.js
+++ docs/pass.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function updateSlider() {
+  var curMinVal = document.querySelector(".min_word").value;
+  document.querySelector("#min_word_val").innerHTML = curMinVal;
+} // END updateSlider
+
+
+window.onload = updateSlider;
