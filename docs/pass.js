c7858484171118ec74586998e0963055dfe2acc3
Warren <weber767@umn.edu>
2019-10-10 21:24:19 -0500

    working page

--- docs/pass.js
+++ docs/pass.js
@@ -1,9 +1,117 @@
 'use strict';
 
-function updateSlider() {
+var wrd_list = [];
+
+function updateSliders() {
   var curMinVal = document.querySelector(".min_word").value;
   document.querySelector("#min_word_val").innerHTML = curMinVal;
+  var curMaxVal = document.querySelector(".max_word").value;
+  document.querySelector("#max_word_val").innerHTML = curMaxVal;
 } // END updateSlider
 
+function getWord() {
+  //set up variables
+  var curMinVal = document.querySelector(".min_word").value;
+  var curMaxVal = document.querySelector(".max_word").value;
+  var maxPassLen = document.querySelector(".pass_lngth").value;
+  var selWord = document.querySelector(".words");
 
-window.onload = updateSlider;
+  //build word list that fits min max sliders
+  if (curMaxVal >= curMinVal) {
+    for (var word in dictionary) {
+      var str = dictionary[word];
+      if (str.length >= curMinVal && str.length <= curMaxVal) {
+        wrd_list.push(str);
+      }
+    }
+  } else {
+    for (var word in dictionary) {
+      var str = dictionary[word];
+      if (str.length >= curMinVal) {
+        wrd_list.push(str);
+      }
+    }
+  }
+
+  // easy typing filter
+  var easy_check = document.querySelector(".easy_wrd");
+  if(easy_check.checked) {
+    for (var wrd = 0; wrd < wrd_list.length; wrd++) {
+      var str = wrd_list[wrd];
+      var easy_val = 0;
+      for (var n = 0; n < str.length - 1; n++) {
+        if( str[n] == str[n + 1] ) {
+          easy_val+= 1;
+        }
+        for(var k = 0; k < keys.length; k++) {
+          if(keys[k].letter == str[n]) {
+            var p1 = keys[k].hand ;
+          }
+          if (keys[k].letter == str[n + 1 ]) {
+            var p2 = keys[k].hand ;
+          }
+        }
+        if(p1 != p2) {
+          easy_val++;
+        }
+      }
+      var ease_score = easy_val / (str.length - 1);
+      if(ease_score < 0.5) {
+        console.log("popping " + str + " score was: " + ease_score);
+        wrd_list.splice(wrd,1);
+      }
+    }
+  }
+
+  // num replacement filter
+  var sub_check = document.querySelector(".num_sub");
+  if(sub_check.checked) {
+    for (var wrd = 0; wrd < wrd_list.length; wrd++) {
+      var rep_wrd = wrd_list[wrd];
+      rep_wrd = rep_wrd.replace(/e/g, "3");
+      rep_wrd = rep_wrd.replace(/l/g, "1");
+      rep_wrd = rep_wrd.replace(/o/g, "0");
+      rep_wrd = rep_wrd.replace(/b/g, "8");
+      rep_wrd = rep_wrd.replace(/t/g, "7");
+      wrd_list[wrd] = rep_wrd;
+    }
+  }
+
+  // create the password table
+  var l = wrd_list.length;
+  var tbl = document.querySelector(".pass_table");
+  for (var j = 0; j < 10; j++) {
+    var pass_lngth = 0;
+    var tr = document.createElement("tr");
+    tbl.appendChild(tr);
+    var pLngth = document.createElement("td");
+    pLngth.innerHTML = pass_lngth;
+    tr.appendChild(pLngth);
+    while (pass_lngth < maxPassLen) {
+      var ran = Math.floor((Math.random() * l));
+      var str = wrd_list[ran];
+      pass_lngth += str.length;
+      if (pass_lngth <= maxPassLen){
+        pLngth.innerHTML = pass_lngth;
+        var td = document.createElement("td");
+        td.innerHTML = str;
+        tr.appendChild(td);
+      }
+    }
+    tr.classList.add("tab");
+    pLngth.classList.add("pass-l");
+  }
+  // Set up table and button features
+  document.querySelector(".pass_btn").disabled = true;
+}
+
+window.onload = updateSliders;
+//window.onload = updateMaxSlider;
+
+var keys = [{letter:"a", hand:"l"}, {letter:"b", hand:"r"}, {letter:"c", hand:"l"}, {letter:"d", hand:"l"}, {letter:"e", hand:"l"},
+            {letter:"f", hand:"l"}, {letter:"g", hand:"l"}, {letter:"h", hand:"r"}, {letter:"i", hand:"r"}, {letter:"j", hand:"r"},
+            {letter:"k", hand:"r"}, {letter:"l", hand:"r"}, {letter:"m", hand:"r"}, {letter:"n", hand:"r"}, {letter:"o", hand:"r"},
+            {letter:"p", hand:"r"}, {letter:"q", hand:"l"}, {letter:"r", hand:"l"}, {letter:"s", hand:"l"}, {letter:"t", hand:"l"},
+            {letter:"u", hand:"r"}, {letter:"v", hand:"l"}, {letter:"w", hand:"l"}, {letter:"x", hand:"l"}, {letter:"y", hand:"r"},
+            {letter:"z", hand:"l"}]
+var dictionary = ['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it', 'you', 'that', 'he', 'was', 'for', 'on', 'are', 'with', 'as', 'I', 'his', 'they', 'be', 'at', 'one', 'have', 'this', 'from', 'or', 'had', 'by', 'hot', 'word', 'but', 'what', 'some', 'we', 'can', 'out', 'other', 'were', 'all', 'there', 'when', 'up', 'use', 'your', 'how', 'said', 'an', 'each', 'she', 'which', 'do', 'their', 'time', 'if', 'will', 'way', 'about', 'many', 'then', 'them', 'write', 'would', 'like', 'so', 'these', 'her', 'long', 'make', 'thing', 'see', 'him', 'two', 'has', 'look', 'more', 'day', 'could', 'go', 'come', 'did', 'number', 'sound', 'no', 'most', 'people', 'my', 'over', 'know', 'water', 'than', 'call', 'first', 'who', 'may', 'down', 'side', 'been', 'now', 'find', 'any', 'new', 'work', 'part', 'take', 'get', 'place', 'made', 'live', 'where', 'after', 'back', 'little', 'only', 'round', 'man', 'year', 'came', 'show', 'every', 'good', 'me', 'give', 'our', 'under', 'name', 'very', 'through', 'just', 'form', 'sentence', 'great', 'think', 'say', 'help', 'low', 'line', 'differ', 'turn', 'cause', 'much', 'mean', 'before', 'move', 'right', 'boy', 'old', 'too', 'same', 'tell', 'does', 'set', 'three', 'want', 'air', 'well', 'also', 'play', 'small', 'end', 'put', 'home', 'read', 'hand', 'port', 'large', 'spell', 'add', 'even', 'land', 'here', 'must', 'big', 'high', 'such', 'follow', 'act', 'why', 'ask', 'men', 'change', 'went', 'light', 'kind', 'off', 'need', 'house', 'picture', 'try', 'us', 'again', 'animal', 'point', 'mother', 'world', 'near', 'build', 'self', 'earth', 'father', 'head', 'stand', 'own', 'page', 'should', 'country', 'found', 'answer', 'school', 'grow', 'study', 'still', 'learn', 'plant', 'cover', 'food', 'sun', 'four', 'between', 'state', 'keep', 'eye', 'never', 'last', 'let', 'thought', 'city', 'tree', 'cross', 'farm', 'hard', 'start', 'might', 'story', 'saw', 'far', 'sea', 'draw', 'left', 'late', 'run', "don't", 'while', 'press', 'close', 'night', 'real', 'life', 'few', 'north', 'open', 'seem', 'together', 'next', 'white', 'children', 'begin', 'got', 'walk', 'example', 'ease', 'paper', 'group', 'always', 'music', 'those', 'both', 'mark', 'often', 'letter', 'until', 'mile', 'river', 'car', 'feet', 'care', 'second', 'book', 'carry', 'took', 'science', 'eat', 'room', 'friend', 'began', 'idea', 'fish', 'mountain', 'stop', 'once', 'base', 'hear', 'horse', 'cut', 'sure', 'watch', 'color', 'face', 'wood', 'main', 'enough', 'plain', 'girl', 'usual', 'young', 'ready', 'above', 'ever', 'red', 'list', 'though', 'feel', 'talk', 'bird', 'soon', 'body', 'dog', 'family', 'direct', 'pose', 'leave', 'song', 'measure', 'door', 'product', 'black', 'short', 'numeral', 'class', 'wind', 'question', 'happen', 'complete', 'ship', 'area', 'half', 'rock', 'order', 'fire', 'south', 'problem', 'piece', 'told', 'knew', 'pass', 'since', 'top', 'whole', 'king', 'space', 'heard', 'best', 'hour', 'better', 'true', 'during', 'hundred', 'five', 'remember', 'step', 'early', 'hold', 'west', 'ground', 'interest', 'reach', 'fast', 'verb', 'sing', 'listen', 'six', 'table', 'travel', 'less', 'morning', 'ten', 'simple', 'several', 'vowel', 'toward', 'war', 'lay', 'against', 'pattern', 'slow', 'center', 'love', 'person', 'money', 'serve', 'appear', 'road', 'map', 'rain', 'rule', 'govern', 'pull', 'cold', 'notice', 'voice', 'unit', 'power', 'town', 'fine', 'certain', 'fly', 'fall', 'lead', 'cry', 'dark', 'machine', 'note', 'wait', 'plan', 'figure', 'star', 'box', 'noun', 'field', 'rest', 'correct', 'able', 'pound', 'done', 'beauty', 'drive', 'stood', 'contain', 'front', 'teach', 'week', 'final', 'gave', 'green', 'oh', 'quick', 'develop', 'ocean', 'warm', 'free', 'minute', 'strong', 'special', 'mind', 'behind', 'clear', 'tail', 'produce', 'fact', 'street', 'inch', 'multiply', 'nothing', 'course', 'stay', 'wheel', 'full', 'force', 'blue', 'object', 'decide', 'surface', 'deep', 'moon', 'island', 'foot', 'system', 'busy', 'test', 'record', 'boat', 'common', 'gold', 'possible', 'plane', 'stead', 'dry', 'wonder', 'laugh', 'thousand', 'ago', 'ran', 'check', 'game', 'shape', 'equate', 'hot', 'miss', 'brought', 'heat', 'snow', 'tire', 'bring', 'yes', 'distant', 'fill', 'east', 'paint', 'language', 'among', 'grand', 'ball', 'yet', 'wave', 'drop', 'heart', 'am', 'present', 'heavy', 'dance', 'engine', 'position', 'arm', 'wide', 'sail', 'material', 'size', 'vary', 'settle', 'speak', 'weight', 'general', 'ice', 'matter', 'circle', 'pair', 'include', 'divide', 'syllable', 'felt', 'perhaps', 'pick', 'sudden', 'count', 'square', 'reason', 'length', 'represent', 'art', 'subject', 'region', 'energy', 'hunt', 'probable', 'bed', 'brother', 'egg', 'ride', 'cell', 'believe', 'fraction', 'forest', 'sit', 'race', 'window', 'store', 'summer', 'train', 'sleep', 'prove', 'lone', 'leg', 'exercise', 'wall', 'catch', 'mount', 'wish', 'sky', 'board', 'joy', 'winter', 'sat', 'written', 'wild', 'instrument', 'kept', 'glass', 'grass', 'cow', 'job', 'edge', 'sign', 'visit', 'past', 'soft', 'fun', 'bright', 'gas', 'weather', 'month', 'million', 'bear', 'finish', 'happy', 'hope', 'flower', 'clothe', 'strange', 'gone', 'jump', 'baby', 'eight', 'village', 'meet', 'root', 'buy', 'raise', 'solve', 'metal', 'whether', 'push', 'seven', 'paragraph', 'third', 'shall', 'held', 'hair', 'describe', 'cook', 'floor', 'either', 'result', 'burn', 'hill', 'safe', 'cat', 'century', 'consider', 'type', 'law', 'bit', 'coast', 'copy', 'phrase', 'silent', 'tall', 'sand', 'soil', 'roll', 'temperature', 'finger', 'industry', 'value', 'fight', 'lie', 'beat', 'excite', 'natural', 'view', 'sense', 'ear', 'else', 'quite', 'broke', 'case', 'middle', 'kill', 'son', 'lake', 'moment', 'scale', 'loud', 'spring', 'observe', 'child', 'straight', 'consonant', 'nation', 'dictionary', 'milk', 'speed', 'method', 'organ', 'pay', 'age', 'section', 'dress', 'cloud', 'surprise', 'quiet', 'stone', 'tiny', 'climb', 'cool', 'design', 'poor', 'lot', 'experiment', 'bottom', 'key', 'iron', 'single', 'stick', 'flat', 'twenty', 'skin', 'smile', 'crease', 'hole', 'trade', 'melody', 'trip', 'office', 'receive', 'row', 'mouth', 'exact', 'symbol', 'die', 'least', 'trouble', 'shout', 'except', 'wrote', 'seed', 'tone', 'join', 'suggest', 'clean', 'break', 'lady', 'yard', 'rise', 'bad', 'blow', 'oil', 'blood', 'touch', 'grew', 'cent', 'mix', 'team', 'wire', 'cost', 'lost', 'brown', 'wear', 'garden', 'equal', 'sent', 'choose', 'fell', 'fit', 'flow', 'fair', 'bank', 'collect', 'save', 'control', 'decimal', 'gentle', 'woman', 'captain', 'practice', 'separate', 'difficult', 'doctor', 'please', 'protect', 'noon', 'whose', 'locate', 'ring', 'character', 'insect', 'caught', 'period', 'indicate', 'radio', 'spoke', 'atom', 'human', 'history', 'effect', 'electric', 'expect', 'crop', 'modern', 'element', 'hit', 'student', 'corner', 'party', 'supply', 'bone', 'rail', 'imagine', 'provide', 'agree', 'thus', 'capital', "won't", 'chair', 'danger', 'fruit', 'rich', 'thick', 'soldier', 'process', 'operate', 'guess', 'necessary', 'sharp', 'wing', 'create', 'neighbor', 'wash', 'bat', 'rather', 'crowd', 'corn', 'compare', 'poem', 'string', 'bell', 'depend', 'meat', 'rub', 'tube', 'famous', 'dollar', 'stream', 'fear', 'sight', 'thin', 'triangle', 'planet', 'hurry', 'chief', 'colony', 'clock', 'mine', 'tie', 'enter', 'major', 'fresh', 'search', 'send', 'yellow', 'gun', 'allow', 'print', 'dead', 'spot', 'desert', 'suit', 'current', 'lift', 'rose', 'continue', 'block', 'chart', 'hat', 'sell', 'success', 'company', 'subtract', 'event', 'particular', 'deal', 'swim', 'term', 'opposite', 'wife', 'shoe', 'shoulder', 'spread', 'arrange', 'camp', 'invent', 'cotton', 'born', 'determine', 'quart', 'nine', 'truck', 'noise', 'level', 'chance', 'gather', 'shop', 'stretch', 'throw', 'shine', 'property', 'column', 'molecule', 'select', 'wrong', 'gray', 'repeat', 'require', 'broad', 'prepare', 'salt', 'nose', 'plural', 'anger', 'claim', 'continent', 'oxygen', 'sugar', 'death', 'pretty', 'skill', 'women', 'season', 'solution', 'magnet', 'silver', 'thank', 'branch', 'match', 'suffix', 'especially', 'fig', 'afraid', 'huge', 'sister', 'steel', 'discuss', 'forward', 'similar', 'guide', 'experience', 'score', 'apple', 'bought', 'led', 'pitch', 'coat', 'mass', 'card', 'band', 'rope', 'slip', 'win', 'dream', 'evening', 'condition', 'feed', 'tool', 'total', 'basic', 'smell', 'valley', 'nor', 'double', 'seat', 'arrive', 'master', 'track', 'parent', 'shore', 'division', 'sheet', 'substance', 'favor', 'connect', 'post', 'spend', 'chord', 'fat', 'glad', 'original', 'share', 'station', 'dad', 'bread', 'charge', 'proper', 'bar', 'offer', 'segment', 'slave', 'duck', 'instant', 'market', 'degree', 'populate', 'chick', 'dear', 'enemy', 'reply', 'drink', 'occur', 'support', 'speech', 'nature', 'range', 'steam', 'motion', 'path', 'liquid', 'log', 'meant', 'quotient', 'teeth', 'shell', 'neck'];
